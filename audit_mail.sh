e244517a8e2045d35e12f1771859888c77b9f795
Aadithya <45553342+1601-1998-7@users.noreply.github.com>
2022-05-17 12:14:00 +0530

    Update audit_mail.sh

--- audit_mail.sh
+++ audit_mail.sh
@@ -4,7 +4,7 @@ SUBJECT="Testing mail whether sbin/sendmail works in  $hostName"
 
         MSG=`echo "Hi Team. This is an automated message from Aadi"`
 
-             /usr/sbin/sendmail -C /etc/mail/sendmail.cf  -f aadithya.t@zohocorp.com -t aadithya.t@zohocorp.com,relevant-teams@zohocorp.com <<END
+             /usr/sbin/sendmail -C /etc/mail/sendmail.cf  -f aadithya.t@company.com -t aadithya.t@company.com,relevant-teams@company.com <<END
 
 SUBJECT:$SUBJECT
 
